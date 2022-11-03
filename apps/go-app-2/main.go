f61d8f044eca0c8f54c655bb79f2ab8863765860
Gary Justin <7003114+gejustin@users.noreply.github.com>
2022-11-03 09:37:40 -0400

    add dependency for go-app-2

--- apps/go-app-2/main.go
+++ apps/go-app-2/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	go_lib_2 "go-lib-2"
+)
 
 func Hello(name string) string {
 	result := "Hello " + name
@@ -8,5 +11,6 @@ func Hello(name string) string {
 }
 
 func main() {
+	fmt.Println(go_lib_2.GoLib2("go-app-2"))
 	fmt.Println(Hello("go-app-2"))
 }
